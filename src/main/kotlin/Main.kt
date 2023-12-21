7fd7c886eab775d9314ea0134a776a36ae7f84dd
andre-l-pinheiro <a.apinheiro@ext.ppay.me>
2023-12-21 08:48:45 -0300

    Subindo o primeiro código convertido do php

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,94 +1,15 @@
 package org.example
 
-import java.math.BigDecimal
-import kotlin.math.pow
-
-
 fun main() {
-    val payload = Payload("yCYv", 64)
-    val result = ManipuleBaseFactory(payload).createBase().converterToDecimal();
-    println(result)
+    loadData()
+        .stream()
+        .forEach{
+            val payload = Payload(it, 64)
+            val result = ManipuleBaseFactory(payload).createBase().converterToDecimal()
+            println("Elemento: $it e resultado: $result")
+        }
 }
 
-fun base64Execute() {
-    println("Executar em base 64")
-
-    val result1 = baseBase2Dec("DLZvS", 64)
-    val result2 = baseBase2Dec("DL6dA", 64)
-    val result3 = baseBase2Dec("x,R0", 64)
-    val result4 = baseBase2Dec("DL8bn", 64)
-    val result5 = baseBase2Dec("x5FK", 64)
-    println(result1)
-    println(result2)
-    println(result3)
-    println(result4)
-    println(result5)
-}
-
-fun baseBase2Dec(sNum: String, iBase: Int = 0, iScale: Int = 0): String {
-    var sChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-    var sResult = BigDecimal.ZERO
-
-    var base = iBase
-
-    if (base == 64)
-        sChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789*,"
-
-    when (base) {
-        16 -> {
-            if (sNum.lowercase().startsWith("0x"))
-                sNum.substring(2)
-        }
-        8 -> {
-            if (sNum.startsWith("0"))
-                sNum.substring(1)
-        }
-        2 -> {
-            if (sNum.lowercase().startsWith("0b"))
-                sNum.substring(2)
-        }
-        64 -> {
-            sNum.replace("=", "")
-        }
-        else -> {
-            var sNum = sNum
-
-            if ("#" in sNum) {
-                val (sBase, newSNum) = sNum.split("#", limit = 2)
-                base = sBase.toInt()
-                newSNum.also { sNum = it }
-            }
-
-            if (base == 0) {
-                println("baseBase2Dec called without a base value and not in base#number format")
-                return ""
-            }
-        }
-    }
-
-    var sNum = sNum
-
-    if (base < 37) sNum = sNum.lowercase()
-
-    if ("." in sNum) {
-        val (newSNum, sReal) = sNum.split(".", limit = 2)
-        sNum = newSNum
-        val real = "0.$sReal"
-        sResult += BigDecimal(real)
-    }
-
-    val iLen = sNum.length
-
-    for (i in iLen - 1 downTo 0) {
-        val sChar = sNum[i].toString()
-        val iValue = sChars.indexOf(sChar)
-        if (iValue > base) {
-            println("baseBase2Dec> $sNum is not a valid base $base number")
-            return ""
-        }
-
-        sResult += BigDecimal(iValue) * base.toDouble().pow((iLen - i - 1).toDouble()).toBigDecimal()
-    }
-
-    return sResult.toString()
+fun loadData(): List<String> {
+    return listOf("xzRj", "DMa4E", "xvnh", "DMbBa", "DMUEK", "yDl*", "DLwGH", "DMa4U", "DMadK", "BMym", "DMMsF")
 }
